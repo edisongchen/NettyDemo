5267006c4d094c5e47031be42ab9f2916dbd5514
ctg <1097744635@qq.com>
2016-11-09 13:41:34 +0800

    修改包结构

--- src/main/java/com/netty/proj/NettyDemo/nio/TimeClient.java
+++ src/main/java/com/netty/proj/NettyDemo/nio/TimeClient.java
@@ -1,16 +1,5 @@
 package com.netty.proj.NettyDemo.nio;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.ClosedChannelException;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.SocketChannel;
-import java.util.Iterator;
-
-import org.omg.CORBA.SystemException;
-
 public class TimeClient {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/netty/proj/NettyDemo/util/Constants.java
@@ -0,0 +1,8 @@
+package com.netty.proj.NettyDemo.util;
+
+/**
+ * Created by ctg on 2016/11/9.
+ */
+public class Constants {
+    public static final String LINE_SEPARATOR="&";
+}
