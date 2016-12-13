d62dbd395bfc5b94a7325a64315dd50590e4fee5
ctg <1097744635@qq.com>
2016-12-13 21:50:51 +0800

    添加笔记

--- src/main/java/com/netty/proj/NettyDemo/netty/n05/EchoServer.java
+++ src/main/java/com/netty/proj/NettyDemo/netty/n05/EchoServer.java
@@ -32,9 +32,9 @@ public class EchoServer {
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
-//                            ByteBuf delimiter = Unpooled.copiedBuffer(Constants.DELIMITER_$_.getBytes());
-//                            ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024,delimiter));
-                            ch.pipeline().addLast(new FixedLengthFrameDecoder(20));
+                            ByteBuf delimiter = Unpooled.copiedBuffer(Constants.DELIMITER_$_.getBytes());
+                            ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024,delimiter));
+//                            ch.pipeline().addLast(new FixedLengthFrameDecoder(20));
                             ch.pipeline().addLast(new StringDecoder());
                             ch.pipeline().addLast(new EchoServerHandler());
                         }
